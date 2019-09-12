62c0fc4b2110ca01799a1cda42a71e45679e25df
james <jneeley@robinsage.com>
2019-09-12 16:21:27 -0600

    added colors to tableVertices, add matrix to adjust for aspectRatio

---
+++ GLProject4/app/src/main/java/com/example/jamesneeley/glproject375/util/TextResourceReader.kt
@@ -0,0 +1,40 @@
+package com.example.jamesneeley.glproject375.util
+
+import android.content.Context
+import android.content.res.Resources
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.RuntimeException
+import java.lang.StringBuilder
+
+object TextResourceReader {
+
+    fun readResourceFromID(context: Context, id: Int): String {
+        val body = StringBuilder()
+
+        try {
+            val inputString = context.resources.openRawResource(id)
+            val inputStreamReader = InputStreamReader(inputString)
+            val bufferedReader = BufferedReader(inputStreamReader)
+
+
+            var line: String?
+            do {
+                line = bufferedReader.readLine()
+                if (line == null) {
+                    break
+                }
+                body.append(line)
+                body.append("\n")
+            } while (true)
+
+
+        } catch (error: IOException) {
+            throw RuntimeException("problem reading resource")
+        } catch (nfe: Resources.NotFoundException) {
+            throw RuntimeException("Resource not found")
+        }
+        return body.toString()
+    }
+}
---
+++ GLProject6/app/src/main/java/com/example/jamesneeley/glproject6/util/ShaderHelper.kt
@@ -0,0 +1,84 @@
+package com.example.jamesneeley.glproject6.util
+
+import android.opengl.GLES30
+import android.util.Log
+object ShaderHelper {
+
+    fun compileVertexShader(shaderCode: String): Int {
+        return compileShader(GLES30.GL_VERTEX_SHADER, shaderCode)
+    }
+
+    fun compileFragmentShader(shaderCode: String): Int {
+        return compileShader(GLES30.GL_FRAGMENT_SHADER, shaderCode)
+    }
+
+    private fun compileShader(type: Int, shaderCode: String): Int {
+        val shaderHandle = GLES30.glCreateShader(type)
+
+        if (shaderHandle == 0 ) {
+            if (LoggerConfig.ON) {
+                Log.w(LoggerConfig.TAG, "Unable to create shader of type: $type")
+            }
+            return 0
+        }
+
+        GLES30.glShaderSource(shaderHandle, shaderCode)
+        GLES30.glCompileShader(shaderHandle)
+
+        val compileStatus = IntArray(1)
+        GLES30.glGetShaderiv(shaderHandle, GLES30.GL_COMPILE_STATUS, compileStatus, 0)
+
+        if (compileStatus[0] == 0) {
+            GLES30.glDeleteShader(shaderHandle)
+            if (LoggerConfig.ON) {
+                Log.w(LoggerConfig.TAG, "Unable to compile shader")
+            }
+            return 0
+        }
+
+        return shaderHandle
+    }
+
+    fun linkProgram(vertexID: Int, fragmentID: Int): Int {
+        val program = GLES30.glCreateProgram()
+
+        if (program == 0) {
+            if (LoggerConfig.ON) {
+                Log.w(LoggerConfig.TAG, "Unable to create program")
+            }
+            return 0
+        }
+
+        GLES30.glAttachShader(program, vertexID)
+        GLES30.glAttachShader(program, fragmentID)
+        GLES30.glLinkProgram(program)
+
+        val linkStatus = IntArray(1)
+        GLES30.glGetProgramiv(program, GLES30.GL_LINK_STATUS, linkStatus, 0)
+        if (linkStatus[0] == 0) {
+            GLES30.glDeleteProgram(program)
+            if (LoggerConfig.ON) {
+                Log.w(LoggerConfig.TAG, "Unable to link program")
+            }
+            return 0
+        }
+        return program
+    }
+
+
+    fun validateProgram(program: Int): Int {
+
+        GLES30.glValidateProgram(program)
+
+        val validateStatus = IntArray(1)
+        GLES30.glGetProgramiv(program, GLES30.GL_VALIDATE_STATUS, validateStatus, 0)
+
+        if (validateStatus[0] == 0) {
+            if (LoggerConfig.ON) {
+                Log.w(LoggerConfig.TAG, "Program not valid: Status - ${validateStatus[0]}. Log: ${GLES30.glGetProgramInfoLog(program)}")
+            }
+        }
+
+        return validateStatus[0]
+    }
+}
